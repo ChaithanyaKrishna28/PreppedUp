99173939e9615e88ca294f57baae128e987cee1c
Chaithanya Krishna <chaithanyakrishna4112@gmail.com>
2025-03-04 23:08:35 +0530

    Changes : Header Component Completed

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom"
 
 export const MainLayout = () => {
   return (
-    <div className="w-full">
+    <div  className="flex flex-col h-screen">
         <Header/>
 
         <Container className="flex-grow">
--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -6,7 +6,7 @@ export const PublicLayout = () => {
   return (
     <div className="w-full">
         {/* handler to store the user data because we need to make sure the userm is authenticated or not we need to store the users data into the firebase if the user is not exist, or if he is already exists we don't need to save that again */}
-
+        
         <Header/>
 
         <Outlet/>
