f7fd9c5418ac08d22d18ae4ef17b30448ac96fb4
Chaithanya Krishna <chaithanyakrishna4112@gmail.com>
2025-03-05 20:00:24 +0530

    Changes : Home Screen Completed

--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,8 +1,10 @@
-import Footer from "@/components/footer"
+import { Footer } from "@/components/footer"
 import Header from "@/components/header"
 import AuthHandler from "@/handlers/auth-handler"
 import { Outlet } from "react-router-dom"
 
+
+// footer brooo if you want to change it go for it
 export const PublicLayout = () => {
   return (
     <div className="w-full">
